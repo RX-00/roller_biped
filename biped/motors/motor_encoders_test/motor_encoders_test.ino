6b99d110d418bb2dd11e897385ea640e2d44b46e
RX-00 <00royxing@gmail.com>
2020-04-13 01:36:10 -0400

    encoders working but need to debug and probably rewire the motors, start with one each and then test all

--- biped/motors/motor_encoders_test/motor_encoders_test.ino
+++ biped/motors/motor_encoders_test/motor_encoders_test.ino
@@ -1,5 +1,16 @@
-// Test code for the encoders, modified from another source
-// Encoder pins definition
+// Test with encoders and motors
+// NOTE: as of April 12, 2020 the encoders seem fine but the motor commands seem to be incorrect
+//       compared to the truth table
+
+// Right Motor  Pins
+#define INA_1 PA_3
+#define INB_1 PA_4
+#define PWM_1 PC_6
+
+// Left Motor Pins
+#define INA_2 PE_4
+#define INB_2 PE_5
+#define PWM_2 PC_5
 
 // Left Encoder
 #define Left_Encoder_PinA PF_4
@@ -18,15 +29,50 @@ volatile long Right_Encoder_Ticks = 0;
 volatile bool RightEncoderBSet;
 
 
-
+//====== SETUP FUNCTION ======
 void setup(){
 	// Init Serial port with 115200 buad rate
   Serial.begin(115200);
-	SetupEncoders();
+	setupEncoders();
+  setupMotors();
 }
 
 
-void SetupEncoders(){
+//====== MAIN LOOP ======
+void loop(){
+  updateEncoders();
+
+  // NOTE: TODO YOU NEED TO CHECK EACH THREE SET
+  //       OF MOTOR PINS AGAIN, CHECK ONE MOTOR SET
+  //       AT A TIME!!
+  // testing out the right motor (1)
+
+  analogWrite(PWM_1, 20); // input PWM signal
+  analogWrite(PWM_2, 20);
+  digitalWrite(INA_1, HIGH);
+  digitalWrite(INB_1, LOW);
+  //digitalWrite(INA_1, HIGH);   // input direction signals (refer to truth table)
+  //digitalWrite(INB_1, HIGH);
+}
+
+
+
+//====== MOTOR FUNCTIONS ======
+void setupMotors(){
+  // Right Motor
+  pinMode(INA_1, OUTPUT);
+  pinMode(INB_1, OUTPUT);
+  pinMode(PWM_1, OUTPUT);
+
+  // Left Motor
+  pinMode(INA_2, OUTPUT);
+  pinMode(INB_2, OUTPUT);
+  pinMode(PWM_2, OUTPUT);
+}
+
+
+//====== ENCODER FUNCTIONS ======
+void setupEncoders(){
   // Quadrature encoders
   // Left encoder
   pinMode(Left_Encoder_PinA, INPUT);      // sets pin A pullup
@@ -40,13 +86,7 @@ void SetupEncoders(){
   attachInterrupt(digitalPinToInterrupt(Right_Encoder_PinA), do_Right_Encoder, RISING);
 }
 
-
-void loop(){
-	Update_Encoders();
-}
-
-
-void Update_Encoders(){
+void updateEncoders(){
   Serial.print("e");
   Serial.print("\t");
   Serial.print(Left_Encoder_Ticks);
