4278d42a0999f89da4f41df9bb56511e764c4663
RX-00 <00royxing@gmail.com>
2020-05-11 05:21:02 +0100

    time to link up the libs and write up that new com class and then work on the IP_balance sub-project

---
+++ biped/main/snsrs_com/src/test.cpp
@@ -0,0 +1,81 @@
+/* Test program for communication of all data from sensors, and micro controller comms
+ * with the main computer
+ *
+ * NOTE: This is using UART Serial protocol at a baud rate of 115200 with default
+ *       8N1 format, which is 8 data bits, no parity, 1 stop bit
+ *
+ * NOTE: data form for motor speed:
+ *                 l(+/-)###r(+/-)###
+ *
+ * TODO: Figure out how to get the RTIMU library linked up correctly
+ */
+
+#include "Com.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include <string>
+
+#include <unistd.h>
+#include <sys/timeb.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "rs232.h"
+#include "RTIMULib.h"
+
+#define BUF_SIZE 128
+#define PORT_NUM 24 // NOTE: using ttyACM0 -> port number 24
+#define BAUDRATE 115200
+
+
+int main(int argc, char** argv){
+  // IMU setup
+
+
+
+  // Communication TX & RX setup
+  Com comms(BUF_SIZE, PORT_NUM, BAUDRATE);
+  char mode[] = {'8', 'N', '1', 0}; // 8 data bits, no parity, 1 stop bit
+  char str_send[1][BUF_SIZE];
+  unsigned char str_recv[BUF_SIZE];
+
+  if (RS232_OpenComport(PORT_NUM, BAUDRATE, mode, 0)){ // 0 is no flowctrl
+    printf("Cannot open port\n");
+    return 0;
+  }
+
+  usleep(500000); // usec -> 500ms for stable condition
+
+  for(int j = 0; j < 10; j++){ // NOTE: This would go on forever in the main loop
+    // dummy velocity data
+    comms.setSpd((rand() % (255 * 2 + 1) + (-255)), (rand() % (255 * 2 + 1) + (-255)));
+    comms.formatData();
+
+    // then put it into str_send
+    strcpy(str_send[0], comms.data.c_str());
+
+    RS232_cputs(PORT_NUM, str_send[0]); // sends string on serial
+    usleep(100000); // waits for reply 100ms
+
+    // gets chars from serial port (if any)
+    int n = RS232_PollComport(PORT_NUM, str_recv, (int)BUF_SIZE);
+    if(n > 0){
+      str_recv[n] = 0; // always put a "null" at the end of a string
+      std::string RX_data((char *)str_recv);
+      comms.interpretRXData(RX_data);
+    }
+    usleep(100000); // sleep for 100ms
+  }
+
+  RS232_CloseComport(PORT_NUM);
+
+  std::cout << "\n\nGot left encoder val: " << comms.getLeftEncoder() << std::endl;
+  std::cout << "Got right encoder val:" << comms.getRightEncoder() << std::endl;
+  std::cout << "Got usec time: " << comms.getTimeUsec() << std::endl;
+  std::cout << "Got sec time: " << comms.getTimeSec() << std::endl;
+
+  return 0;
+}
